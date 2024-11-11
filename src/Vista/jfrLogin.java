98fe288eb2c6be9f29a1cbe8b95276d2c97860d6
Fer <Correo>
2024-11-11 13:18:13 -0300

    metodo init

--- src/Vista/jfrLogin.java
+++ src/Vista/jfrLogin.java
@@ -349,7 +349,7 @@ public class jfrLogin extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new jfrLogin().setVisible(true);
+                initjfrLogin();
             }
         });
     }
